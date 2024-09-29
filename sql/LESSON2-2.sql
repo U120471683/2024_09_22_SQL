de0ca78c01d4c1445e42d7d07c60ef8be4be77c1
U120471683 <shichanliaw@gmail.com>
2024-09-29 15:31:04 +0800

    SDFSDFGSD

--- sql/LESSON2-2.sql
+++ sql/LESSON2-2.sql
@@ -1,10 +1,10 @@
 DROP TABLE IF EXISTS hospital;
 CREATE TABLE IF NOT EXISTS hospital (    
-    序號	SERIAL PRIMARY KEY,
-    直轄市或省轄縣市	VARCHAR(5) NOT NULL,
-    醫療機構名稱	VARCHAR(30) NOT NULL unique,
-    市話	VARCHAR(15) ,
-    分機	VARCHAR(7) ,
-    聯絡人	VARCHAR(7) ,
-    地址	VARCHAR(50) 
+    ID	SERIAL PRIMARY KEY,
+    HUALIEN	VARCHAR(5) NOT NULL,
+    HOSPITAL	VARCHAR(30) NOT NULL unique,
+    TELEPHONE	VARCHAR(15) ,
+    ST	VARCHAR(7) ,
+    PERSON	VARCHAR(7) ,
+    HOME	VARCHAR(50) 
 );
