08506f338fa0a3b7eacd920a182336a1834304fc
U120471683 <shichanliaw@gmail.com>
2024-09-29 21:21:32 +0800

    sdsdfsdf

--- sql/LESSON2-2.sql
+++ sql/LESSON2-2.sql
@@ -9,3 +9,16 @@ CREATE TABLE IF NOT EXISTS hospital (
 	contact VARCHAR(7),
 	address VARCHAR(50)
 );
+/*
+id -> 主鍵,自動遞增值
+code -> 字串 -> 不可以重複 -> 不可以是空的
+name -> 字串 -> 不可以重覆
+e_name -> 字串 -> 不可以重覆 */
+
+CREATE TABLE IF NOT EXISTS TRA_station_code (
+    id SERIAL PRIMARY KEY,
+    code VARCHAR(4) NOT NULL,
+	name VARCHAR(10) NOT NULL UNIQUE,
+	e_name VARCHAR(30),
+	
+);
