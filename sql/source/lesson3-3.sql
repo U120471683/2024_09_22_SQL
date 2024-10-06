24cbf0b939e638424ae907183e3fbdfddec8ffda
U120471683 <shichanliaw@gmail.com>
2024-10-06 11:40:13 +0800

    dsfsdf

---
+++ sql/source/lesson3-3.sql
@@ -0,0 +1,18 @@
+SELECT id as 序號,code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM tra_station_code
+WHERE code = '1001';
+
+SELECT id as 序號,code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM tra_station_code
+WHERE code = '1001' OR code = '1003';
+
+SELECT id as 序號,code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM tra_station_code
+WHERE code IN  ('1001' ,  '1003');
+
+SELECT id as 序號,code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM tra_station_code
+WHERE code = '1001' OR name='八堵';
+
+
+
