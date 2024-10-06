353724048f5b6838dfac5ea2dd62c0d8ca4584c8
U120471683 <shichanliaw@gmail.com>
2024-10-06 12:02:55 +0800

    dfsdfsdfs

--- sql/source/lesson3-3.sql
+++ sql/source/lesson3-3.sql
@@ -14,5 +14,28 @@ SELECT id as 序號,code as 代碼,name as 車站名稱,e_name as 英文名稱
 FROM tra_station_code
 WHERE code = '1001' OR name='八堵';
 
+SELECT id as 序號,code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM tra_station_code
+/*WHERE name= '基隆'; */
+WHERE name LIKE '台_';
 
+SELECT id as 序號,code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM tra_station_code
+/*WHERE name= '基隆'; */
+WHERE name LIKE '台%';
+
+SELECT id as 序號,code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM tra_station_code
+/*WHERE name= '基隆'; */
+WHERE name LIKE '%港';
+
+SELECT id as 序號,code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM tra_station_code
+/*WHERE name= '基隆'; */
+WHERE e_name ILIKE '%p%';
+
+SELECT id as 序號,code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM tra_station_code
+/*WHERE name= '基隆'; */
+WHERE id<=20 AND id>=10;
 
