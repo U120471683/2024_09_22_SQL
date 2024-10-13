1365f22606b90f9b921abae1c09509a82fcfa228
U120471683 <shichanliaw@gmail.com>
2024-10-13 19:20:12 +0800

    dfhdfhdf

--- sql/lesson4/lesson4-5.sql
+++ sql/lesson4/lesson4-5.sql
@@ -12,3 +12,14 @@ HAVING SUM(amount) > 200;
 SELECT store_id ,COUNT(customer_id) AS 數量
 FROM customer
 GROUP BY store_id;
+
+SELECT * FROM country
+
+SELECT country_id 
+FROM country
+WHERE country='Taiwan';
+
+SELECT city
+FROM city
+WHERE country_id='92';
+
