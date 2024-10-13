f68375209e4c34eca391da8b28f9c1b41c78e4d9
U120471683 <shichanliaw@gmail.com>
2024-10-13 16:10:57 +0800

    dsfsdfsdf

--- sql/lesson4/lesson4-5.sql
+++ sql/lesson4/lesson4-5.sql
@@ -1,4 +1,14 @@
-SELECT cusmoter_id,SUM(amount) as 總合
+SELECT *
+FROM payment;
+
+
+SELECT customer_id,SUM(amount) AS 總合
 FROM payment
 GROUP BY customer_id
 HAVING SUM(amount) > 200;
+
+
+
+SELECT store_id ,COUNT(customer_id) AS 數量
+FROM customer
+GROUP BY store_id;
