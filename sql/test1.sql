76f223184cc420c0ddadfecc0d9051953156fb65
U120471683 <shichanliaw@gmail.com>
2024-10-19 21:59:56 +0800

    dfgdfgdf

---
+++ sql/test1.sql
@@ -0,0 +1,5 @@
+SELECT stationname as 站名, sum(gateincomingcnt) as 進站總人數
+FROM station_in_out in_out JOIN stations s on in_out.stacode=s.stationcode
+WHERE EXTRACT(YEAR from date)='2022'
+GROUP BY stationname
+ORDER BY SUM(gateincomingcnt) DESC;
