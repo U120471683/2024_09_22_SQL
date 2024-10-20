7ec959ca3298d7757296df3007f052ec7864cc93
U120471683 <shichanliaw@gmail.com>
2024-10-20 10:19:17 +0800

    dfsdfsdf

--- sql/lesson5/lesson5-2.sql
+++ sql/lesson5/lesson5-2.sql
@@ -1,4 +1,5 @@
 /*全省各站點2022年進站總人數大於5佰萬人的站點*/
+/*全省各站點2022年進站總人數大於5佰萬人的站點*/
 SELECT stationname AS 站名, SUM(gateincomingcnt) AS 總進站人數
 FROM stations JOIN station_in_out ON stationcode = stacode
 /*WHERE date BETWEEN '2022-01-01' AND '2022-12-31';*/
