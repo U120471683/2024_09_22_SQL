4dae1a8817d0fac9dc91df3db5825ceac53c29ec
U120471683 <shichanliaw@gmail.com>
2024-11-17 10:27:27 +0800

    fdgdfgfd

---
+++ lesson9/datasource.py
@@ -0,0 +1,16 @@
+# 此為自訂的moudule，用來提供資料源
+# 這裡的資料源是一個常數PI
+
+PI=3.1415926
+
+#   class DataSource:               #
+
+class DataSource:               #
+    def __init__(self):
+        pass
+
+    def get_data(self):
+        return PI
+# funtion
+def get_data():
+    time.sleep(5)
--- lesson9/main.py
+++ lesson9/main.py
@@ -3,12 +3,15 @@ import streamlit as st
 import os
 # import time
 from time import sleep
-
+from datasources import get_data    #從datasources.py中import get_data函數
 load_dotenv()    #m模擬環境變數 
+
+
 placeholder=st.empty()
 with placeholder:
     with st.spinner('下載資料中...'):
-        sleep(5)
+        get_data()
+        #sleep(5)
         # st.success('完成!')           
     st.success('下載完成!',icon='🎉')  #icon='🎉' 代表成功的icon
     sleep(3)
