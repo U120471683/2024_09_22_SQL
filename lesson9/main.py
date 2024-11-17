9880243ea873e30e46521e8f49bac184f06abfd9
U120471683 <shichanliaw@gmail.com>
2024-11-17 11:09:55 +0800

    dfhgfhgfh

--- lesson9/main.py
+++ lesson9/main.py
@@ -3,17 +3,20 @@ import streamlit as st
 import os
 # import time
 from time import sleep
-from datasources import get_data    #從datasources.py中import get_data函數
+from datasource import get_data    #從datasources.py中import get_data函數
+
 load_dotenv()    #m模擬環境變數 
 
 
-placeholder=st.empty()
+placeholder=st.empty() #建立一個空的元素
 with placeholder:
-    with st.spinner('下載資料中...'):
-        get_data()
+    with st.spinner('下載資料中...'):   
+        get_data()      #呼叫datasource.py中的get_data函數  #此函數會sleep 5秒
         #sleep(5)
         # st.success('完成!')           
-    st.success('下載完成!',icon='🎉')  #icon='🎉' 代表成功的icon
+    #st.success('下載完成!',icon='🎉')  #icon='🎉' 代表成功的icon
+    st.success("下載完成",icon=":material/favorite:") #icon=":material/favorite:" 代表成功的icon
     sleep(3)
 
-placeholder.empty()
+placeholder.empty() #清空placeholder
+
---
+++ lesson9/main2.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv      #從dotenv模組中import load_dotenv函數
+import streamlit as st               #import streamlit模組
+from time import sleep                          #從time模組中import sleep函數
+from datasource import get_data         #從datasources.py中import get_data函數              
+load_dotenv()                                #模擬環境變數
+
+
+@st.dialog("目前發生問題,請稍後再試")   #自訂的dialog               
+def alert():                          #定義alert函數                             
+        st.write('連線有問題')  #顯示連線有問題
+        st.stop()        #停止程式
+
+with st.spinner('下載資料中...'):      #顯示spinner                          
+        get_data()                      #呼叫datasource.py中的get_data函數  #此函數會sleep 5秒
+alert()                                     #呼叫alert函數
+st.write('顯示頁面')              #顯示顯示頁面
+
