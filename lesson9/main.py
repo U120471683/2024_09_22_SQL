3b8eeaa5531b00b4afd1dce407678be285834f64
U120471683 <shichanliaw@gmail.com>
2024-11-17 10:19:57 +0800

    sdgsdgs

--- lesson9/main.py
+++ lesson9/main.py
@@ -1,4 +1,16 @@
 from dotenv import load_dotenv
+import streamlit as st
+import os
+# import time
+from time import sleep
 
+load_dotenv()    #m模擬環境變數 
+placeholder=st.empty()
+with placeholder:
+    with st.spinner('下載資料中...'):
+        sleep(5)
+        # st.success('完成!')           
+    st.success('下載完成!',icon='🎉')  #icon='🎉' 代表成功的icon
+    sleep(3)
 
-load_dotenv()    #m模擬環境變數             
+placeholder.empty()
