49b1cc7246023431ecc21169bd08499e1b00844c
U120471683 <shichanliaw@gmail.com>
2024-11-23 17:43:37 +0800

    sdgdfgdfgd

---
+++ lesson9/main5.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+import streamlit as st
+from pprint import pprint
+from tools import get_data
+load_dotenv()
+
+
+@st.dialog("目前發生問題,請稍後再試")
+def alert():
+        st.write('連線有問題')
+        st.stop()
+
+with st.spinner('下載資料中...'):
+        youbikes = get_data()
+        
+if youbikes is None:
+    alert()
+st.write(youbikes)
+
+
+
+    
+
+
+
+    
--- lesson9/tools/__init__.py
+++ lesson9/tools/__init__.py
@@ -1,1 +1,2 @@
-from .__datasource__ import get_data
+# from .__datasource__ import get_data,save_to_database
+from .__datasource__ import *   
